648cf45b4a03edd723748d476ca0c8b2e9edf6de
Felipe Lopes <felipeslopes2010@hotmail.com>
2023-01-18 13:52:40 -0300

    Atualiza testes automáticos

--- cypress/e2e/gui/datePicker.cy.js
+++ cypress/e2e/gui/datePicker.cy.js
@@ -3,54 +3,53 @@ describe('Testa Date Picker QA-Ninja', () => {
   const MES_ANIVERSARIO = '03'
   const DIA_ANIVERSARIO = '17'
   const ANO_ATUAL = new Date().getFullYear()
+  const dataAtual = new Date()
+  const diaAtual = dataAtual.getDate()
+  const diaAniversario = new Date(`${MES_ANIVERSARIO}-${DIA_ANIVERSARIO}-${ANO_ATUAL}`)
+  const diaAniversarioFormatado = new Date(`${MES_ANIVERSARIO}-${DIA_ANIVERSARIO}-${ANO_ATUAL}`).toLocaleDateString(local)
+  const dataAtualFormatada = new Date().toLocaleDateString(local)
 
   beforeEach(() => {
     cy.visit('/datepicker')
   })
   
-  const dataAtual = new Date().toLocaleDateString(local)
   it('Verifica se data atual é preenchida ao entrar no site', () => {
 
-    cy.get('.datetimepicker-dummy-input').should('have.value', dataAtual)
+    cy.get('.datetimepicker-dummy-input').should('have.value', dataAtualFormatada)
   })
 
   it('Limpa Date Picker e trás data atual através do botão Today', () => {
-    const dataAtual = new Date().toLocaleDateString(local)
     
     cy.gui_clicaCalendario()
     cy.get('.datetimepicker-footer-clear').click()
     cy.gui_clicaCalendario()
     cy.get('.datetimepicker-footer-today').click()
 
-    cy.get('.datetimepicker-dummy-input').should('have.value', dataAtual)
+    cy.get('.datetimepicker-dummy-input').should('have.value', dataAtualFormatada)
   })
 
   it('Seleciona data de aniversário', () => {
-    const diaAniversario = new Date(`${MES_ANIVERSARIO}-${DIA_ANIVERSARIO}-${ANO_ATUAL}`).toLocaleDateString(local)
 
     cy.gui_clicaCalendario()
     cy.get('.datepicker-nav').click()
     cy.get(`[data-month=${MES_ANIVERSARIO}]`).click()
     cy.get('.is-current-month').contains(DIA_ANIVERSARIO).click()
 
-    cy.get('.datetimepicker-dummy-input').should('have.value', diaAniversario)
+    cy.get('.datetimepicker-dummy-input').should('have.value', diaAniversarioFormatado)
   })
 
   it('Seleciona dia atual no mês anterior', () => {
-    const dataAtual = new Date()
-    const diaAtual = dataAtual.getDate()
     const mesAnterior = dataAtual.getMonth() - 1;
-    const diaAtualUltimoMes = new Date(dataAtual.getFullYear(), mesAnterior, dataAtual.getDate()).toLocaleDateString(local);
+    const diaAtualUltimoMesFormatado = new Date(dataAtual.getFullYear(), mesAnterior, dataAtual.getDate()).toLocaleDateString(local);
 
     cy.gui_clicaCalendario()
     cy.get('.datepicker-nav-previous').click()
     cy.get('.datepicker-days').contains(diaAtual).click()
 
-    cy.get('.datetimepicker-dummy-input').should('have.value', diaAtualUltimoMes)
+    cy.get('.datetimepicker-dummy-input').should('have.value', diaAtualUltimoMesFormatado)
   })
 
   it('Extra 1: Altera Data para aniversário e valida sua seleção', () => {
-    const diaAniversario = new Date(`${MES_ANIVERSARIO}-${DIA_ANIVERSARIO}-${ANO_ATUAL}`)
     const diaAniversarioFormatado = diaAniversario.toLocaleDateString(local);
 
     cy.clock(diaAniversario)
@@ -59,9 +58,7 @@ describe('Testa Date Picker QA-Ninja', () => {
     cy.get('.datetimepicker-dummy-input').should('have.value', diaAniversarioFormatado)
   })
 
-  it('Extra 2: Altera Data para aniversário do próximo ano e valida sua seleção', () => {
-
-    const diaAniversario = new Date(`${MES_ANIVERSARIO}-${DIA_ANIVERSARIO}-${ANO_ATUAL}`)
+  it('Extra 2: Altera Data para aniversário do próximo ano e valida sua seleção', () => {    
     const aniversarioProximoAno = diaAniversario.getFullYear() + 1
     const proximoAniversario = new Date(`${MES_ANIVERSARIO}-${DIA_ANIVERSARIO}-${aniversarioProximoAno}`)
     const proximoAniversarioFormatado = proximoAniversario.toLocaleDateString(local)
