b7556a0b1bd12c7adf4dffa0e7301424951a64f0
Felipe Lopes <felipeslopes2010@hotmail.com>
2023-01-18 13:43:07 -0300

    Adiciona novos comandos

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -2,8 +2,12 @@ Cypress.Commands.add('gui_realizaLogin', user => {
     cy.get('input[type="text"]').type(user.login, {delay: 0})
     cy.get('input[type=password]').type(user.password, { log: false, delay: 0 })
     cy.get('button[type="submit"]').click()
-  });
+  })
 
   Cypress.Commands.add('gui_clicaCalendario', () => { 
     cy.get('.datetimepicker-dummy-input').click()
-  });
+  })
+
+  Cypress.Commands.add('gui_clicaVoltar', () => {
+    cy.contains('Voltar').click()
+  })
