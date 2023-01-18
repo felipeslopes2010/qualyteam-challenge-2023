bd10f6a71ad6318de046c4bfd3002a4b215e7003
Felipe Lopes <felipeslopes2010@hotmail.com>
2023-01-18 01:23:28 -0300

    Corrige Variaveis Ambiente

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://training-wheels-qaninja.herokuapp.com',
-    user_name: "",
-    user_password: ""
     },
   },
 );
