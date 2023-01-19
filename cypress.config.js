33c4e7110242c22e42155f359256e52a7f74c3de
Felipe Lopes <felipeslopes2010@hotmail.com>
2023-01-19 20:32:22 -0300

    Adiciona configuração para rodar todos os testes

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://training-wheels-qaninja.herokuapp.com',
-    },
+    experimentalRunAllSpecs: true,
+  },
   fixturesFolder: false,
   video: false,
-  },
+},
 );
